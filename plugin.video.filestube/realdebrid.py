998142da0e6af8dfe9797e8f99379000570ec967
Daan <daanmathot@gmail.com>
2013-11-04 21:32:25 +0100

    cleaner auth error handling

--- plugin.video.filestube/realdebrid.py
+++ plugin.video.filestube/realdebrid.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
 # dydrmntion@gmail.com
 
+import re
 
 import requests
-from cookielib import CookieJar
 
+
+class RealDebridAuthError(Exception):
+    pass
 
 class Api(object):
     base_url = 'http://real-debrid.com/ajax'
     def __init__(self, user, password, persistent_cookie=False):
         self.user = user
         self.password = password
+        if len(self.user) == 0 or len(self.password) == 0:
+            raise RealDebridAuthError('Invalid login settings')
 
     def get_cookies(self):
         req = requests.get(self.base_url + '/login.php', params={
             'user': self.user,
             'pass': self.password})
-        if req.ok and not req.json()['error']:
-            return req.cookies
+        if req.ok:
+            resp = req.json()
+            if not resp['error']:
+                return req.cookies
+            emessage = re.sub(r'\s+|\n', ' ', resp['message'])
+            raise RealDebridAuthError(emessage)
 
     def unrestrict_link(self, link):
         req = requests.get(self.base_url + '/unrestrict.php', params={
